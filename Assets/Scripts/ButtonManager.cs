b0f44ede1246ff1a2f5cb0af7772d74b7059111d
Tyler <118314063+CaptainLobster4@users.noreply.github.com>
2025-02-23 16:34:00 -0500

    fixed button discrepancy. added layer and edited npc game objects

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,15 +6,34 @@ using UnityEngine.EventSystems;
 
 public class ButtonManager : MonoBehaviour
 {
-    public int ItemID;
-    public Text PriceText;
-    public Text antAmountText;
-    public GameObject ShopManagerVar;
+    public int ItemID; // 1 for Worker Ant, 2 for Soldier Ant
+    public Text PriceText; // To show the price of the item
+    public Text antAmountText; // To show the number of ants purchased
+    public GameObject ShopManagerVar; // Reference to the ShopManager
 
-    // Update is called once per frame
+    private int workerAntCost = 5; // Cost for Worker Ant
+    private int soldierAntCost = 10; // Cost for Soldier Ant
+
     void Update()
     {
-        PriceText.text = "Food: " + ShopManagerVar.GetComponent<ShopManager>().shopItems[2, ItemID].ToString();
-        antAmountText.text = "Ants: " + ShopManagerVar.GetComponent<ShopManager>().shopItems[3, ItemID].ToString();
+        // Update PriceText based on the ItemID (1 = Worker Ant, 2 = Soldier Ant)
+        if (ItemID == 1)
+        {
+            PriceText.text = "Food: " + workerAntCost.ToString(); // Show Worker Ant price
+        }
+        else if (ItemID == 2)
+        {
+            PriceText.text = "Food: " + soldierAntCost.ToString(); // Show Soldier Ant price
+        }
+
+        // Update antAmountText based on the amount of ants purchased (using FoodManager to get the count)
+        if (ItemID == 1)
+        {
+            antAmountText.text = "Ants: " + ShopManagerVar.GetComponent<FoodManager>().AntCount.ToString(); // Worker Ant count
+        }
+        else if (ItemID == 2)
+        {
+            antAmountText.text = "Ants: " + ShopManagerVar.GetComponent<FoodManager>().AntCount.ToString(); // Soldier Ant count
+        }
     }
 }
